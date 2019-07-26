e4bd809a818c622aede3d2f525b7325d0eebaf40
Jinhang Zhang <1297380425@qq.com>
2019-07-26 13:13:54 +0800

    add student

--- ExamSystem1/src/main/java/com/neuedu/demo/controller/AdminController.java
+++ ExamSystem1/src/main/java/com/neuedu/demo/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.neuedu.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.neuedu.demo.domain.Admin;
