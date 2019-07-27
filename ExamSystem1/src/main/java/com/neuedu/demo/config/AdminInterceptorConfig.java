4dcab6bf5cf8d46a7f6a4ede5b825f11363c27a1
renjiejie <38006578+renjiejie@users.noreply.github.com>
2019-07-27 14:56:38 +0800

    小修改

--- ExamSystem1/src/main/java/com/neuedu/demo/config/AdminInterceptorConfig.java
+++ ExamSystem1/src/main/java/com/neuedu/demo/config/AdminInterceptorConfig.java
@@ -5,9 +5,7 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
-import com.neuedu.demo.interceptor.AdminInterceptor;
+	import com.neuedu.demo.interceptor.AdminInterceptor;
 import com.neuedu.demo.interceptor.StudentInterceptor;
 import com.neuedu.demo.interceptor.TeacherInterceptor;
 import com.neuedu.demo.interceptor.UserInterceptor;
--- ExamSystem1/src/main/java/com/neuedu/demo/dao/StudentMapper.java
+++ ExamSystem1/src/main/java/com/neuedu/demo/dao/StudentMapper.java
@@ -1,12 +1,15 @@
 package com.neuedu.demo.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.neuedu.demo.domain.Exam;
 import com.neuedu.demo.domain.Student;
 
 @Mapper
 public interface StudentMapper {
 
-	public Exam joinExam(@Param("id") String id, @Param("password") String password);
+//	public List<Exam> joinExam();
 }
