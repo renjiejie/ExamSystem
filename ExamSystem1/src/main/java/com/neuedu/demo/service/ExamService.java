3554bc0e9f481137d6fffd2739bbd355d77d03d1
Jinhang Zhang <1297380425@qq.com>
2019-07-30 19:38:19 +0800

    老师批改第一层页面完成

---
+++ ExamSystem1/src/main/java/com/neuedu/demo/service/ExamService.java
@@ -0,0 +1,9 @@
+package com.neuedu.demo.service;
+
+import java.util.List;
+
+import com.neuedu.demo.domain.Exam;
+
+public interface ExamService {
+	public List<Exam> getExams(String courseId);
+}
---
+++ ExamSystem1/src/main/java/com/neuedu/demo/service/ExamServiceImpl.java
@@ -0,0 +1,23 @@
+package com.neuedu.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neuedu.demo.dao.ExamMapper;
+import com.neuedu.demo.dao.StudentMapper;
+import com.neuedu.demo.domain.Exam;
+
+@Service
+public class ExamServiceImpl implements ExamService{
+	@Autowired
+	private ExamMapper examMapper;
+
+	@Override
+	public List<Exam> getExams(String courseId) {
+		return examMapper.getExams(courseId);
+	}
+	
+	
+}
